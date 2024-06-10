00fbd761c31c5c6b630258c41b565e08a9d1f0c8
REDY <redy.wijaya24@gmail.com>
2024-06-10 20:57:27 +0700

    detail article binding

--- app/src/main/java/com/capstone/hibeauty/article/DetailArticleActivity.kt
+++ app/src/main/java/com/capstone/hibeauty/article/DetailArticleActivity.kt
@@ -9,49 +9,35 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.capstone.hibeauty.R
+import com.capstone.hibeauty.databinding.ActivityDetailArticleBinding
 
 class DetailArticleActivity : AppCompatActivity() {
-    private lateinit var title: TextView
-    private lateinit var description: TextView
-    private lateinit var publishedAt: TextView
-    private lateinit var author: TextView
-    private lateinit var image: ImageView
-    private lateinit var content: TextView
-    private lateinit var openInBrowserButton: Button
-    private lateinit var closeButton: FloatingActionButton
+
+    private lateinit var binding: ActivityDetailArticleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_article)
+        binding = ActivityDetailArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
-
-        // Temukan komponen UI
-        title = findViewById(R.id.title)
-        description = findViewById(R.id.description)
-        publishedAt = findViewById(R.id.published_at)
-        author = findViewById(R.id.author)
-        image = findViewById(R.id.image)
-        content = findViewById(R.id.content)
-        openInBrowserButton = findViewById(R.id.open_in_browser)
-        closeButton = findViewById(R.id.close_button)
 
         // Terima data artikel dari Intent
         val article = intent.getSerializableExtra("article") as? ArticlesItem
 
-        // Atur nilai untuk komponen UI
-        title.text = article?.title
-        description.text = article?.description
-        publishedAt.text = article?.publishedAt
-        author.text = "Author: ${article?.author ?: "Unknown"}"
-        content.text = article?.content
+        // Atur nilai untuk komponen UI menggunakan ViewBinding
+        binding.title.text = article?.title
+        binding.description.text = article?.description
+        binding.publishedAt.text = article?.publishedAt
+        binding.author.text = "Author: ${article?.author ?: "Unknown"}"
+        binding.content.text = article?.content
 
         article?.urlToImage?.let {
-            Glide.with(this).load(it).into(image)
+            Glide.with(this).load(it).into(binding.image)
         }
 
         // Menambahkan listener untuk membuka artikel di browser
-        openInBrowserButton.setOnClickListener {
+        binding.openInBrowser.setOnClickListener {
             article?.url?.let { url ->
                 val browserIntent = Intent(Intent.ACTION_VIEW, android.net.Uri.parse(url))
                 startActivity(browserIntent)  // Membuka browser dengan URL artikel
@@ -59,7 +45,7 @@ class DetailArticleActivity : AppCompatActivity() {
         }
 
         // Menambahkan listener untuk tombol close
-        closeButton.setOnClickListener {
+        binding.closeButton.setOnClickListener {
             finish()  // Menutup aktivitas saat tombol close ditekan
         }
     }
